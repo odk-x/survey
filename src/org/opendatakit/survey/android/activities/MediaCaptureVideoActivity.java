83d69c6305d18f3d97482a212fbd1fdfe047693e
clarlars <clarlars@gmail.com>
2014-05-22 16:10:42 -0700

    Get rid of MyCameraApp for Nexus 7 Android 4.3 fix

--- src/org/opendatakit/survey/android/activities/MediaCaptureVideoActivity.java
+++ src/org/opendatakit/survey/android/activities/MediaCaptureVideoActivity.java
@@ -313,15 +313,14 @@ public class MediaCaptureVideoActivity extends Activity {
       // To be safe, you should check that the SDCard is mounted
       // using Environment.getExternalStorageState() before doing this.
 
-      File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+      File mediaStorageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
       // This location works best if you want the created images to be shared
       // between applications and persist after your app has been uninstalled.
 
       // Create the storage directory if it does not exist
       if (! mediaStorageDir.exists()){
           if (! mediaStorageDir.mkdirs()){
-              Log.d("MyCameraApp", "failed to create directory");
+              Log.d(t, "failed to create directory");
               return null;
           }
       }
