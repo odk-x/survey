f8779a13253c6e77f0236b2530c3b724cda86405
wbrunette <wbrunette@gmail.com>
2017-02-16 13:58:31 -0800

    refactor common code to load webkit on ui thread

--- survey_app/src/main/java/org/opendatakit/survey/views/OdkSurveyWebView.java
+++ survey_app/src/main/java/org/opendatakit/survey/views/OdkSurveyWebView.java
@@ -11,72 +11,69 @@ import org.opendatakit.survey.activities.IOdkSurveyActivity;
  * @author mitchellsundt@gmail.com
  */
 public class OdkSurveyWebView extends ODKWebView {
-  private static final String t = "OdkSurveyWebView";
+   private static final String t = "OdkSurveyWebView";
 
-  private OdkSurvey odkSurvey;
+   private OdkSurvey odkSurvey;
 
-  public OdkSurveyWebView(Context context, AttributeSet attrs) {
-    super(context, attrs);
+   public OdkSurveyWebView(Context context, AttributeSet attrs) {
+      super(context, attrs);
 
-    // stomp on the odkSurvey object...
-    odkSurvey = new OdkSurvey((IOdkSurveyActivity) context, this);
-    addJavascriptInterface(odkSurvey.getJavascriptInterfaceWithWeakReference(), "odkSurvey");
-  }
+      // stomp on the odkSurvey object...
+      odkSurvey = new OdkSurvey((IOdkSurveyActivity) context, this);
+      addJavascriptInterface(odkSurvey.getJavascriptInterfaceWithWeakReference(), "odkSurvey");
+   }
 
-  @Override
-  public boolean hasPageFramework() {
-    return true;
-  }
+   @Override public boolean hasPageFramework() {
+      return true;
+   }
 
-  @Override
-  public synchronized void loadPage() {
-    /**
-     * NOTE: Reload the web framework only if it has changed.
-     */
+   /**
+    * IMPORTANT: This function should only be called with the context of the database listeners
+    * OR if called from elsewhere there should be an if statement before invoking that checks
+    * if the database is currently available.
+    */
+   @Override public synchronized void loadPage() {
+      /**
+       * NOTE: Reload the web framework only if it has changed.
+       */
 
-    if ( ((IOdkDataActivity) getContext()).getDatabase() == null ) {
-      // do not initiate reload until we have the database set up...
-      return;
-    }
+      log.i(t, "loadPage: current loadPageUrl: " + getLoadPageUrl());
+      String baseUrl = ((IOdkSurveyActivity) getContext())
+          .getUrlBaseLocation(hasPageFrameworkFinishedLoading() && getLoadPageUrl() != null);
+      String hash = ((IOdkSurveyActivity) getContext()).getUrlLocationHash();
 
-    log.i(t, "loadPage: current loadPageUrl: " + getLoadPageUrl());
-    String baseUrl = ((IOdkSurveyActivity) getContext()).getUrlBaseLocation(
-        hasPageFrameworkFinishedLoading() && getLoadPageUrl() != null);
-    String hash = ((IOdkSurveyActivity) getContext()).getUrlLocationHash();
+      if (baseUrl != null) {
+         // for Survey, we do care about the URL
+         String fullUrl = baseUrl + hash;
 
-    if ( baseUrl != null ) {
-      // for Survey, we do care about the URL
-      String fullUrl = baseUrl + hash;
+         loadPageOnUiThread(fullUrl, false);
 
-      loadPageOnUiThread(fullUrl, false);
+      } else if (hasPageFrameworkFinishedLoading()) {
+         log.i(t, "loadPage: delegate to gotoUrlHash: " + hash);
+         gotoUrlHash(hash);
+      } else {
+         log.w(t, "loadPage: framework did not load -- cannot load anything!");
+      }
+   }
 
-    } else if ( hasPageFrameworkFinishedLoading() ) {
-      log.i(t,  "loadPage: delegate to gotoUrlHash: " + hash);
-      gotoUrlHash(hash);
-    } else {
-      log.w(t, "loadPage: framework did not load -- cannot load anything!");
-    }
-  }
+   /**
+    * IMPORTANT: This function should only be called with the context of the database listeners
+    * OR if called from elsewhere there should be an if statement before invoking that checks
+    * if the database is currently available.
+    */
+   @Override public synchronized void reloadPage() {
 
-  @Override
-  public synchronized void reloadPage() {
-    if ( ((IOdkDataActivity) getContext()).getDatabase() == null ) {
-      // do not initiate reload until we have the database set up...
-      return;
-    }
+      log.i(t, "reloadPage: current loadPageUrl: " + getLoadPageUrl());
+      String baseUrl = ((IOdkSurveyActivity) getContext()).getUrlBaseLocation(false);
+      String hash = ((IOdkSurveyActivity) getContext()).getUrlLocationHash();
 
-    log.i(t, "reloadPage: current loadPageUrl: " + getLoadPageUrl());
-    String baseUrl = ((IOdkSurveyActivity) getContext()).getUrlBaseLocation(false);
-    String hash = ((IOdkSurveyActivity) getContext()).getUrlLocationHash();
-
-    if ( baseUrl != null ) {
-      // for Survey, we do care about the URL
-      String fullUrl = baseUrl + hash;
-       loadPageOnUiThread(fullUrl, true);
-    } else {
-      log.w(t, "reloadPage: framework did not load -- cannot load anything!");
-    }
-  }
-
+      if (baseUrl != null) {
+         // for Survey, we do care about the URL
+         String fullUrl = baseUrl + hash;
+         loadPageOnUiThread(fullUrl, true);
+      } else {
+         log.w(t, "reloadPage: framework did not load -- cannot load anything!");
+      }
+   }
 
 }
